0efb7c7bb735a0c1fb82549d1d086d3eaab710cc
Claire Karsenti <claire.karsenti.web@gmail.com>
2022-10-12 15:19:37 +0200

    Added some unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,44 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Footer from './components/Footer';
+import Card from './components/Card';
+import TagBar from './components/TagBar';
 
+// App component
 test('renders App component correctly', () => {
   render(<App />);
   const company = screen.getByText(/Photosnap/i);
   expect(company).toBeInTheDocument();
+});
+
+// Footer component
+test('renders Header component correctly', () => {
+  render(<Footer />);
+  screen.debug();
+
+  const websiteLink = screen.getByText(/Claire Karsenti/i);
+  expect(websiteLink).toBeInTheDocument();
+  expect(websiteLink).toHaveAttribute(
+    'href',
+    'https://www.linkedin.com/in/claire-karsenti/'
+  );
+  expect(websiteLink).toHaveStyle('color: var(--very-dark-grayish-cyan)');
+});
+
+// Card component
+test('renders Card component correctly', () => {
+  render(<Card jobs={[]} handleFilters={undefined} />);
+  screen.debug();
+});
+
+// TagBar component
+test('renders TagBar component correctly', () => {
+  render(
+    <TagBar
+      filters={[]}
+      removeFilter={undefined}
+      clearAllFilters={undefined}
+    />
+  );
+  screen.debug();
 });
--- src/components/Card.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Card from './Card';
-
-test('renders Card component correctly', () => {
-  render(<Card jobs={[]} handleFilters={undefined} />);
-  screen.debug();
-});
