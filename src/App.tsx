2cc9369bc124fb1f08a132c2370c14be7d027bdf
Claire Karsenti <claire.karsenti.web@gmail.com>
2022-10-16 21:31:06 +0200

    Fixed the position of the container when using filters

--- src/App.tsx
+++ src/App.tsx
@@ -51,6 +51,7 @@ function App() {
 
   return (
     <div className="wrapper">
+      <header className="top-img"></header>
       <div className="container">
         <TagBar
           filters={filters}
